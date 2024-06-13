50b60258256989c72e4766dc109988718d8a96ea
Illia Malovanyi <96831202+pantemon@users.noreply.github.com>
2024-06-13 02:46:05 +0100

    feat(contracts): refactor logger

--- contracts/tests/utils.ts
+++ contracts/tests/utils.ts
@@ -1,101 +1,6 @@
 import { Contract, Transaction } from "@ton/core";
 import { SandboxContract } from "@ton/sandbox";
 
-const decimalCount = 9;
-const decimal = pow10(decimalCount);
-
-function pow10(n: number): bigint {
-  let v = 1n;
-  for (let i = 0; i < n; i++) {
-    v *= 10n;
-  }
-  return v;
-}
-
-function formatCoinsPure(value: bigint, precision = 6): string {
-  let whole = value / decimal;
-
-  let frac = value % decimal;
-  const precisionDecimal = pow10(decimalCount - precision);
-  if (frac % precisionDecimal > 0n) {
-    // round up
-    frac += precisionDecimal;
-    if (frac >= decimal) {
-      frac -= decimal;
-      whole += 1n;
-    }
-  }
-  frac /= precisionDecimal;
-
-  return `${whole.toString()}${
-    frac !== 0n
-      ? "." + frac.toString().padStart(precision, "0").replace(/0+$/, "")
-      : ""
-  }`;
-}
-
-function formatCoins(value: bigint | undefined | null, precision = 6): string {
-  if (value === undefined || value === null) return "N/A";
-
-  return formatCoinsPure(value, precision) + " TON";
-}
-
-export function printTransactions(
-  info: string,
-  transactions: Transaction[],
-) {
-  console.info(info);
-
-  const formattedTransactions = transactions.map((tx) => {
-    if (tx.description.type !== "generic") return undefined;
-
-    const body = tx.inMessage?.info.type === "internal"
-      ? tx.inMessage.body?.beginParse()
-      : undefined;
-    const op = body && body.remainingBits >= 32
-      ? body.preloadUint(32)
-      : body
-      ? "no body"
-      : "N/A";
-
-    const totalFees = formatCoins(tx.totalFees.coins);
-    const computeFees = tx.description.computePhase.type === "vm"
-      ? formatCoins(tx.description.computePhase.gasFees)
-      : undefined;
-    const totalFwdFees = formatCoins(tx.description.actionPhase?.totalFwdFees);
-    const valueIn = tx.inMessage?.info.type === "internal"
-      ? formatCoins(tx.inMessage.info.value.coins)
-      : undefined;
-
-    const valuesOut = tx.outMessages.values()
-      .filter((message) => message.info.type === "internal")
-      // @ts-ignore
-      .map((message) => formatCoins(message.info.value.coins))
-      .join("\n");
-
-    const forwardIn = tx.inMessage?.info.type === "internal"
-      ? formatCoins(tx.inMessage.info.forwardFee)
-      : undefined;
-
-    return {
-      src: tx.inMessage?.info.src ?? "Tooth Fairy",
-      dest: tx.inMessage?.info.dest,
-      type: tx.inMessage?.info.type,
-      op: typeof op === "number" ? "0x" + op.toString(16) : op,
-      valueIn,
-      valuesOut,
-      totalFees,
-      outActions: tx.description.actionPhase?.totalActions ?? "N/A",
-      exitCode: tx.description.computePhase.type === "vm"
-        ? tx.description.computePhase.exitCode
-        : "N/A",
-      actionCode: tx.description.actionPhase?.resultCode ?? "N/A",
-    };
-  }).filter((v) => v !== undefined);
-
-  console.table(formattedTransactions);
-}
-
 export class ShrekLogger {
   private contractLabels: Map<string, string>;
 
@@ -126,7 +31,7 @@ export class ShrekLogger {
     const label = this.contractLabels.get(address);
 
     if (!label) {
-      return address;
+      return shortenString(address);
     }
 
     return label;
@@ -206,3 +111,60 @@ export class ShrekLogger {
     };
   }
 }
+
+// BEGIN HELPERS
+
+function shortenString(str: string): string {
+  if (str.length <= 8) {
+    return str; // If the string is 8 characters or less, return it as is.
+  }
+
+  const firstPart = str.slice(0, 4); // Get the first 4 characters.
+  const lastPart = str.slice(-4); // Get the last 4 characters.
+
+  return `${firstPart}...${lastPart}`;
+}
+
+const decimalCount = 9;
+const decimal = pow10(decimalCount);
+
+function pow10(n: number): bigint {
+  let v = 1n;
+  for (let i = 0; i < n; i++) {
+    v *= 10n;
+  }
+  return v;
+}
+
+function formatCoinsPure(value: bigint, precision = 6): string {
+  let whole = value / decimal;
+
+  let frac = value % decimal;
+  const precisionDecimal = pow10(decimalCount - precision);
+  if (frac % precisionDecimal > 0n) {
+    // round up
+    frac += precisionDecimal;
+    if (frac >= decimal) {
+      frac -= decimal;
+      whole += 1n;
+    }
+  }
+  frac /= precisionDecimal;
+
+  return `${whole.toString()}${
+    frac !== 0n
+      ? "." + frac.toString().padStart(precision, "0").replace(/0+$/, "")
+      : ""
+  }`;
+}
+
+export function formatCoins(
+  value: bigint | undefined | null,
+  precision = 6,
+): string {
+  if (value === undefined || value === null) return "N/A";
+
+  return `${formatCoinsPure(value, precision)} TON`;
+}
+
+// END HELPERS
