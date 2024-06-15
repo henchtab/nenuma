b87fb89d96300572890c7715784317f5a744b1e6
Illia Malovanyi <96831202+pantemon@users.noreply.github.com>
2024-06-16 00:49:10 +0100

    refactor(contracts): temporary remove cash-or-nothing option as it's not used

--- contracts/wrappers/CashOrNothingOption.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from "@ton/blueprint";
-
-export const compile: CompilerConfig = {
-  lang: "tact",
-  target: "contracts/cash_or_nothing_option.tact",
-  options: {
-    debug: true,
-  },
-};
