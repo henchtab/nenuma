d57b0f1062dcc2f1ec227d2d21353c83f0141bd8
Illia Malovanyi <96831202+pantemon@users.noreply.github.com>
2024-06-19 22:20:10 +0100

    refactor(contracts): improve shrek

--- contracts/tests/utils.ts
+++ contracts/tests/utils.ts
@@ -1,10 +1,12 @@
 import { Address, Contract, Transaction } from "@ton/core";
-import { SandboxContract } from "@ton/sandbox";
+import { Blockchain, SandboxContract, SmartContract } from "@ton/sandbox";
 
 export class ShrekLogger {
+  private blockchain: Blockchain;
   private contractLabels: Map<string, string>;
 
-  constructor() {
+  constructor(blockchain: Blockchain) {
+    this.blockchain = blockchain;
     this.contractLabels = new Map<string, string>();
   }
 
@@ -16,26 +18,15 @@ export class ShrekLogger {
   addContract<F extends Contract>(
     contract: SandboxContract<F> | Address | string,
     label: string,
-  ) {
-    let address: string;
+  ): void {
+    const address = this.getAddressString(contract);
 
-    // Determine the address string based on the type of the contract parameter.
-    if (typeof contract === "object" && "address" in contract) {
-      address = (contract as SandboxContract<F>).address.toString();
-    } else if (typeof contract === "string") {
-      address = contract;
-    } else {
-      address = contract.toString();
-    }
-
-    // Check for duplicate labels and throw an error if a label already exists.
-    if (this.contractLabels.get(address) === address) {
+    if (this.contractLabels.has(address)) {
       throw new Error(
         `The '${address}' contract already has a '${label}' label!`,
       );
     }
 
-    // Set the label for the given address.
     this.contractLabels.set(address, label);
   }
 
@@ -44,18 +35,32 @@ export class ShrekLogger {
    * @param address The address of the contract.
    * @returns The label associated with the address or a shortened version of the address.
    */
-  getContractLabel(address?: string): string {
-    if (!address) {
-      return "Shrek";
+  getContractLabel(address: Address): string {
+    return this.contractLabels.get(address.toString()) ||
+      shortenString(address.toString());
+  }
+
+  /**
+   * Retrieves the balance for a given contract address.
+   * @param address The address of the contract.
+   * @returns The balance associated with the address.
+   */
+  async getContractBalance(address: Address): Promise<bigint> {
+    try {
+      const contract = await this.blockchain.getContract(address);
+      return contract.balance;
+    } catch (error) {
+      console.error("Error fetching contract balance:", error);
+      throw error;
     }
+  }
 
-    const label = this.contractLabels.get(address);
-
-    if (!label) {
-      return shortenString(address);
-    }
-
-    return label;
+  /**
+   * Logs all contract balances.
+   */
+  async logContracts(): Promise<void> {
+    const data = await this.getContractData();
+    console.table(data);
   }
 
   /**
@@ -63,16 +68,18 @@ export class ShrekLogger {
    * @param transactions The list of transactions to log.
    * @param info Optional info to log before the transactions.
    */
-  logTransactions(transactions: Transaction[], info?: string) {
+  async logTransactions(
+    transactions: Transaction[],
+    info?: string,
+  ): Promise<void> {
     if (info) {
       console.info(info);
     }
 
-    const formattedTransactions = transactions.map((tx) =>
-      this.formatTransaction(tx)
-    ).filter((v) => v !== undefined);
-
-    console.table(formattedTransactions);
+    const formattedTransactions = await Promise.all(
+      transactions.map((tx) => this.formatTransaction(tx)),
+    );
+    console.table(formattedTransactions.filter((tx) => tx !== undefined));
   }
 
   /**
@@ -80,71 +87,92 @@ export class ShrekLogger {
    * @param tx The transaction to format.
    * @returns A formatted transaction object or undefined if not applicable.
    */
-  private formatTransaction(tx: Transaction) {
+  private async formatTransaction(
+    tx: Transaction,
+  ): Promise<object | undefined> {
     if (tx.description.type !== "generic") {
       return undefined;
     }
 
-    // Parse the transaction body if it is an internal message.
     const body = tx.inMessage?.info.type === "internal"
       ? tx.inMessage.body?.beginParse()
       : undefined;
-
-    // Preload the operation code if available.
     const op = body && body.remainingBits >= 32
       ? body.preloadUint(32)
-      : body
-      ? "0xN0B0DY"
       : "0gR33n0gr";
 
-    // Format the transaction fees and values.
-    const totalFees = formatCoins(tx.totalFees.coins);
-    const computeFees = tx.description.computePhase.type === "vm"
-      ? formatCoins(tx.description.computePhase.gasFees)
-      : undefined;
-    const totalFwdFees = formatCoins(
-      tx.description.actionPhase?.totalFwdFees,
-    );
-    const valueIn = tx.inMessage?.info.type === "internal"
-      ? formatCoins(tx.inMessage.info.value.coins)
-      : "The swamp keeps its secrets...";
+    const source = tx.inMessage?.info.src;
+    let sourceLabel = "Shrek";
 
-    const valuesOut = tx.outMessages.values().length == 0
-      ? ("No coins!")
-      : (tx.outMessages.values()
-        .filter((message) => message.info.type === "internal")
-        // @ts-ignore
-        .map((message) => formatCoins(message.info.value.coins)));
+    if (source && Address.isAddress(source)) {
+      sourceLabel = this.getContractLabel(source);
+    }
 
-    const forwardIn = tx.inMessage?.info.type === "internal"
-      ? formatCoins(tx.inMessage.info.forwardFee)
-      : undefined;
+    const destination = tx.inMessage?.info.dest;
+    let destinationLabel = "Donkey";
 
-    // Get source and destination labels.
-    let source = this.getContractLabel(
-      tx.inMessage?.info.src?.toString(),
-    );
+    if (destination && Address.isAddress(destination)) {
+      destinationLabel = this.getContractLabel(destination);
+    }
 
-    let destination = this.getContractLabel(
-      tx.inMessage?.info.dest?.toString(),
-    );
-
-    // Return the formatted transaction object.
     return {
-      "Source": source,
-      "Destination": destination,
+      "Source": sourceLabel,
+      "Destination": destinationLabel,
       "Type": tx.inMessage?.info.type,
       "Opcode": typeof op === "number" ? "0x" + op.toString(16) : op,
-      "Incoming Value": valueIn,
-      "Outgoing Value": valuesOut,
-      "Total Fees": totalFees,
+      "Incoming Value": formatCoins(
+        tx.inMessage?.info.type === "internal"
+          ? tx.inMessage.info.value.coins
+          : undefined,
+      ),
+      "Outgoing Value": tx.outMessages.values().length == 0
+        ? "No coins!"
+        : tx.outMessages.values().filter((msg) => msg.info.type === "internal")
+          // @ts-expect-error
+          .map((msg) => formatCoins(msg.info.value.coins)),
+      "Total Fees": formatCoins(tx.totalFees.coins),
       "Outgoing Actions Count": tx.description.actionPhase?.totalActions ??
         "No actions here?",
       "Exit Code": tx.description.computePhase.type === "vm"
         ? tx.description.computePhase.exitCode
         : "Ogres have layers?",
-      // actionCode: tx.description.actionPhase?.resultCode ?? "N/A",
     };
+  }
+
+  /**
+   * Gets the address string from a contract, address, or string.
+   * @param contract The contract or address.
+   * @returns The address as a string.
+   */
+  private getAddressString<F extends Contract>(
+    contract: SandboxContract<F> | Address | string,
+  ): string {
+    if (typeof contract === "object" && "address" in contract) {
+      return (contract as SandboxContract<F>).address.toString();
+    } else if (typeof contract === "string") {
+      return contract;
+    } else {
+      return contract.toString();
+    }
+  }
+
+  /**
+   * Retrieves formatted contract data.
+   * @returns The formatted contract data array.
+   */
+  private async getContractData(): Promise<
+    { Contract: string; "Contract Balance": string }[]
+  > {
+    const data = [];
+    for (const [addressAsString, label] of this.contractLabels) {
+      const address = Address.parse(addressAsString);
+      const balance = await this.getContractBalance(address);
+      data.push({
+        "Contract": label,
+        "Contract Balance": formatCoins(balance),
+      });
+    }
+    return data;
   }
 }
 
@@ -156,14 +184,7 @@ export class ShrekLogger {
  * @returns The shortened string.
  */
 function shortenString(str: string): string {
-  if (str.length <= 8) {
-    return str; // If the string is 8 characters or less, return it as is.
-  }
-
-  const firstPart = str.slice(0, 4); // Get the first 4 characters.
-  const lastPart = str.slice(-4); // Get the last 4 characters.
-
-  return `${firstPart}...${lastPart}`;
+  return str.length <= 8 ? str : `${str.slice(0, 4)}...${str.slice(-4)}`;
 }
 
 const decimalCount = 9;
@@ -190,11 +211,10 @@ function pow10(n: number): bigint {
  */
 function formatCoinsPure(value: bigint, precision = 6): string {
   let whole = value / decimal;
-
   let frac = value % decimal;
   const precisionDecimal = pow10(decimalCount - precision);
+
   if (frac % precisionDecimal > 0n) {
-    // Round up the fractional part.
     frac += precisionDecimal;
     if (frac >= decimal) {
       frac -= decimal;
@@ -220,9 +240,9 @@ export function formatCoins(
   value: bigint | undefined | null,
   precision = 6,
 ): string {
-  if (value === undefined || value === null) return "N/A";
-
-  return formatCoinsPure(value, precision);
+  return value === undefined || value === null
+    ? "N/A"
+    : formatCoinsPure(value, precision);
 }
 
 // END HELPERS
