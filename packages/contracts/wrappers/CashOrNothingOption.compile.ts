97b6e6a06de324a28361f8239de5bca4484c6121
dreamqip <v.chuchupak@gmail.com>
2024-06-24 21:04:43 +0200

    fix(contracts): downgrade tact compiler due to errors

--- packages/contracts/wrappers/CashOrNothingOption.compile.ts
+++ packages/contracts/wrappers/CashOrNothingOption.compile.ts
@@ -4,6 +4,6 @@ export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/cash_or_nothing_option.tact',
     options: {
-        debug: true,
+        debug: false,
     },
 };
--- packages/contracts/wrappers/DataStream.compile.ts
+++ packages/contracts/wrappers/DataStream.compile.ts
@@ -4,6 +4,6 @@ export const compile: CompilerConfig = {
   lang: "tact",
   target: "contracts/data_stream.tact",
   options: {
-    debug: true,
+    debug: false,
   },
 };
