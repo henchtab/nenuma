b9af5cdacaa1dd32b3b8215585a8b80dbb047457
dreamqip <v.chuchupak@gmail.com>
2024-07-02 17:59:01 +0200

    fix(www): don't save subscription batch to localstorage more than once

--- apps/www/src/lib/wrappers/subscription-batch.ts
+++ apps/www/src/lib/wrappers/subscription-batch.ts
@@ -1,4 +1,4 @@
-import { Address, type TonClient4 } from '@ton/ton';
+import { Address, OpenedContract, type TonClient4 } from '@ton/ton';
 import { SubscriptionBatch } from 'nenuma-contracts';
 import type { OpenContract } from '.';
 import { DATA_STREAM_STORAGE_KEY, SUBSCRIPTION_BATCHES_STORAGE_KEY } from '../constants';
@@ -8,40 +8,60 @@ export default class SubscriptionBatchWrapper implements OpenContract<Subscripti
   private readonly publicClient: TonClient4;
   private readonly batchId: bigint;
 
+  private streamAddress?: Address;
+  private subscriptionBatch?: OpenedContract<SubscriptionBatch>;
+
   constructor(publicClient: TonClient4, batchId: bigint) {
     this.publicClient = publicClient;
     this.batchId = batchId;
+
+    const streamAddress = loadData<AddressData>(DATA_STREAM_STORAGE_KEY)?.address;
+
+    if (streamAddress) {
+      this.streamAddress = Address.parse(streamAddress);
+    }
+
+    this.saveSubscriptionBatch();
+  }
+
+  // FIXME: If a batch is not deployed, we still save it address in the local storage
+  // Ideally, we should save the batch only after it is deployed
+  async saveSubscriptionBatch() {
+    if (!this.streamAddress) {
+      return;
+    }
+
+    const newSubscriptionBatch = await SubscriptionBatch.fromInit(this.streamAddress, this.batchId);
+
+    const existingBatches = loadData<AddressData[]>(SUBSCRIPTION_BATCHES_STORAGE_KEY) || [];
+    const existingBatch = existingBatches.findIndex(
+      (batch) => batch.address === newSubscriptionBatch.address.toString()
+    );
+
+    if (existingBatch === -1) {
+      existingBatches.push({
+        address: newSubscriptionBatch.address.toString(),
+        timestamp: Date.now()
+      });
+      localStorage.setItem(SUBSCRIPTION_BATCHES_STORAGE_KEY, JSON.stringify(existingBatches));
+    }
   }
 
   async getOpenedContract() {
-    const streamAddress = loadData<AddressData>(DATA_STREAM_STORAGE_KEY)?.address;
+    if (this.subscriptionBatch) {
+      return this.subscriptionBatch;
+    }
 
-    if (!streamAddress) {
+    if (!this.streamAddress) {
       throw new Error('No stream address found. Did you deploy the data stream?');
     }
 
-    const subscriptionBatch = await SubscriptionBatch.fromInit(
-      Address.parse(streamAddress),
-      this.batchId
-    );
+    const subscriptionBatch = await SubscriptionBatch.fromInit(this.streamAddress, this.batchId);
 
-    const batches = loadData<AddressData[]>(SUBSCRIPTION_BATCHES_STORAGE_KEY) || [];
+    const openedContract = this.publicClient.open(subscriptionBatch);
 
-    if (
-      batches.findIndex(
-        (batch) =>
-          batch.address ===
-          subscriptionBatch.address.toString({ testOnly: true, bounceable: false })
-      ) !== -1
-    ) {
-      batches.push({
-        address: subscriptionBatch.address.toString({ testOnly: true, bounceable: false }),
-        timestamp: Date.now()
-      });
-      localStorage.setItem(SUBSCRIPTION_BATCHES_STORAGE_KEY, JSON.stringify(batches));
-    }
-
-    return this.publicClient.open(subscriptionBatch);
+    this.subscriptionBatch = openedContract;
+    return openedContract;
   }
 
   async getBalance() {
