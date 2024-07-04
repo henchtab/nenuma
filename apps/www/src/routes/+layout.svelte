6f21d41b4f2a2fea7ffaf59c182d93f28d6351d1
dreamqip <v.chuchupak@gmail.com>
2024-07-04 23:59:53 +0200

    fix(www): possible crash cause

--- apps/www/src/routes/+layout.svelte
+++ apps/www/src/routes/+layout.svelte
@@ -23,7 +23,7 @@
   onMount(async () => {
     expandApp();
 
-    postEvent('web_app_set_header_color', { color: '#000' });
+    // postEvent('web_app_set_header_color', { color: '#000' });
 
     tonConnectUI.subscribe(async (tonConnectUI) => {
       if (!tonConnectUI) {
