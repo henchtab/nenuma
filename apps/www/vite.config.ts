fc3f6436ca9c5c493fb2c79b51291bb17b960e2f
dreamqip <v.chuchupak@gmail.com>
2024-10-09 22:16:23 +0200

    fix(www): load env in vite.config.ts

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,22 +1,25 @@
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sentrySvelteKit } from '@sentry/sveltekit';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import mkcert from 'vite-plugin-mkcert';
 
-export default defineConfig({
-  plugins: [
-    enhancedImages(),
-    sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: process.env.SENTRY_ORG,
-        project: process.env.SENTRY_PROJECT,
-        authToken: process.env.SENTRY_AUTH_TOKEN
-      },
-      adapter: 'node',
-    }),
-    sveltekit(),
-    nodePolyfills({ include: ['buffer'] })
-  ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      enhancedImages(),
+      sentrySvelteKit({
+        sourceMapsUploadOptions: {
+          org: env.SENTRY_ORG,
+          project: env.SENTRY_PROJECT,
+          authToken: env.SENTRY_AUTH_TOKEN
+        },
+        adapter: 'node'
+      }),
+      sveltekit(),
+      nodePolyfills({ include: ['buffer'] })
+    ],
+  };
 });
