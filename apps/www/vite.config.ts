b03cdf272bdb5d6111ef21e3a51c0062bee4f0b8
dreamqip <v.chuchupak@gmail.com>
2024-10-09 22:24:23 +0200

    fix(www): load all env regardless of the `VITE_` prefix.

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [
